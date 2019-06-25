9214a35bcc125b0e1e4a2418e2c77f658cade751
kajtazovai <kajtazova@gmail.com>
2019-06-25 23:32:09 +0200

    Create components for edit timereports

---
+++ TimeReporting/src/app/edittimereport/edittimereport.component.ts
@@ -0,0 +1,81 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {Employee} from "../models/employee";
+import {TimeService} from "../services/time.service";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
+import {UserService} from "../services/user.service";
+import {ProjectService} from "../services/project.service";
+import {Project} from "../models/project";
+import {NewtimereportComponent} from "../newtimereport/newtimereport.component";
+import {Timereport} from "../models/timereport";
+
+@Component({
+  selector: 'app-edittimereport',
+  templateUrl: './edittimereport.component.html',
+  styleUrls: ['./edittimereport.component.css']
+})
+export class EdittimereportComponent implements OnInit {
+  hours:number;
+  date:Date;
+  employeeId:number;
+  projectId:number;
+  projectName:string;
+  projects:Object;
+  employees:Array<Employee>;
+  selectedEmployee:Employee;
+  selectedProject: Project;
+  timereport:Timereport;
+
+
+  constructor(private timereportService  : TimeService,public dialogRef: MatDialogRef<NewtimereportComponent>,
+              private employeeService:UserService,private  projectService:ProjectService,@Inject(MAT_DIALOG_DATA) public data: any) {
+
+  }
+
+
+
+  ngOnInit() {
+    this.selectedProject = this.data.projectId;
+    this.hours = this.data.hours;
+    var dateTimereport = new Date(this.data.date);
+    this.employeeId = this.data.employeeId;
+    this.projectId = this.data.projectId;
+    this.date = dateTimereport;
+    this.projectName = this.data.projectName;
+    var currentUser = window.sessionStorage.getItem('user');
+    var parsed = JSON.parse(currentUser);
+    this.selectedEmployee = new Employee(parsed.id,parsed.firstName,parsed.lastName,parsed.username,parsed.password,parsed.email,parsed.dateJoining);
+    this.projects = new Array<Project>();
+    this.projectService.getProjects().subscribe(text=>{
+      this.projects = text;
+    })
+  }
+  onCloseCancel() {
+      this.dialogRef.close('Close');
+  }
+
+  editTimereport() {
+    this.timereport = new Timereport(this.hours,this.employeeId,this.selectedProject.id,this.selectedEmployee.firstname,this.selectedProject.name,this.data.id,this.date);
+    this.timereportService.editTimereport(this.timereport);
+    this.dialogRef.close("Edit");
+    alert("Successfully edited");
+  }
+
+  changeProject(event) {
+    var pom = this.projectService.getProjectById(parseInt(event)).subscribe(text=>{
+      var pom = JSON.stringify(text);
+      var parsed = JSON.parse(pom);
+      console.log(parsed)
+      return this.selectedProject = new Project(parsed.name,parsed.id);
+    });
+  }
+
+  changeDate(event) {
+    var date = new Date(event.toString());
+    this.date=date;
+    console.log(this.date);
+  }
+
+  changeHours(event) {
+    this.hours = event;
+  }
+}
---
+++ TimeReporting/src/app/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import {Route, Router} from "@angular/router";
+import {Role} from "../models/role";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  isLogin:boolean;
+  role:Role;
+  constructor(private route : Router){
+  }
+
+  ngOnInit() {
+    var pom = window.sessionStorage.getItem('user');
+    if(pom!=null){
+      var parsed = JSON.parse(pom);
+      this.role = new Role(parsed.role.id);
+      console.log(this.role);
+      this.isLogin= true;
+    }
+    else{
+      this.isLogin = false;
+      console.log(this.isLogin);
+    }
+  }
+  logoutUser(){
+    window.sessionStorage.removeItem('user');
+    this.route.navigate(['/login']);
+    console.log('Logout')
+  }
+
+}
