220594f1e4c8e620f48cc6028282ec49f323c84d
kajtazovai <kajtazova@gmail.com>
2019-09-30 22:53:22 +0200

    Added new changes

--- TimeReporting/src/app/newtimereport/newtimereport.component.ts
+++ TimeReporting/src/app/newtimereport/newtimereport.component.ts
@@ -1,13 +1,13 @@
-import {Component, Inject, OnInit} from '@angular/core';
-import {Project} from "../models/project";
-import {Timereport} from "../models/timereport";
-import {TimeService} from "../services/time.service";
-import { MatDialogRef ,MatDialogContent, MAT_DIALOG_DATA} from "@angular/material";
-import {Employee} from "../models/employee";
-import {UserService} from "../services/user.service";
-import {ProjectService} from "../services/project.service";
-import {Role} from "../models/role";
-import {FormControl, FormGroup, Validators} from "@angular/forms";
+import { Component, Inject, OnInit } from '@angular/core';
+import { Project } from "../models/project";
+import { Timereport } from "../models/timereport";
+import { TimeService } from "../services/time.service";
+import { MatDialogRef, MatDialogContent, MAT_DIALOG_DATA } from "@angular/material";
+import { Employee } from "../models/employee";
+import { UserService } from "../services/user.service";
+import { ProjectService } from "../services/project.service";
+import { Role } from "../models/role";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 import * as moment from 'moment';
 
@@ -20,60 +20,77 @@ export class NewtimereportComponent implements OnInit {
   newtimereport = new FormGroup({
     hours: new FormControl([
 
-    ],[Validators.required])
+    ], [Validators.required])
   })
-  hours:number;
-  date:Date;
-  employeeId:number;
-  projectId:number;
-  projects:Array<Project>;
-  employees:Array<Employee>;
-  selectedEmployee:Employee;
+  hours: number;
+  date: Date;
+  employeeId: number;
+  projectId: number;
+  projects: Array<Project>;
+  employees: Array<Employee>;
+  selectedEmployee: Employee;
   selectedProject: Project;
-  role:Role;
-  selected:String;
+  role: Role;
+  selected: String;
   projectName = String;
-  haveData:Boolean;
-  constructor(private timereportService  : TimeService,public dialogRef: MatDialogRef<NewtimereportComponent>,private employeeService:UserService,private  projectService:ProjectService,@Inject(MAT_DIALOG_DATA) public data: any) { }
+  haveData: Boolean;
+  constructor(private timereportService: TimeService, public dialogRef: MatDialogRef<NewtimereportComponent>, private employeeService: UserService, private projectService: ProjectService, @Inject(MAT_DIALOG_DATA) public data: any) { }
   ngOnInit() {
-    var session  = window.sessionStorage.getItem('user');
+    var session = window.sessionStorage.getItem('user');
     var parsed = JSON.parse(session);
-    this.role = new Role(parsed.role.id,parsed.role.name);
+    this.role = new Role(parsed.role.id, parsed.role.name);
     this.haveData = false;
-    this.selectedProject = this.data.project;
-   if(this.data!=undefined){
-     this.haveData=true;
-    this.selected = this.data.projectName;
-    this.selectedProject = this.data.project;
-    this.hours = this.data.hours;
-    var dateTimereport = new Date(this.data.date);
-    this.employeeId = this.data.employeeId;
-    this.projectId = this.data.projectId;
-    this.date = dateTimereport;
-    this.projectName = this.data.projectName;
-    this.selectedEmployee = new Employee(parsed.id,parsed.firstName,parsed.lastName,parsed.username,parsed.password,parsed.email,parsed.dateJoining,parsed.embg,parsed.role,parsed.projects);
-    this.projects = new Array<Project>();
-    this.projects = parsed.projects;
-   }else{
-    this.selectedEmployee = new Employee(parsed.id, parsed.firstName, parsed.lastName, parsed.username, parsed.password, parsed.email, parsed.dateJoining,parsed.embg,parsed.role,parsed.projects);
-    this.projects = new Array<Project>();
-    this.employees = new Array<Employee>();
-    this.employeeService.getUsers().subscribe((text:Array<Employee>)=>{
-      this.employees = text;
-    })
-    this.projects = parsed.projects;
-   }
+    if (this.data != undefined) {
+      this.haveData = true;
+      this.selected = this.data.projectName;
+      this.selectedProject = this.data.project;
+      this.hours = this.data.hours;
+      var dateTimereport = new Date(this.data.date);
+      this.employeeId = this.data.employeeId;
+      this.projectId = this.data.projectId;
+      this.date = dateTimereport;
+      this.projectName = this.data.projectName;
+      this.selectedEmployee = new Employee(parsed.id, parsed.firstName, parsed.lastName, parsed.username, parsed.password, parsed.email, parsed.dateJoining, parsed.embg, parsed.role, parsed.projects);
+      this.projects = new Array<Project>();
+      this.projects = parsed.projects;
+    } else {
+      this.employees = new Array<Employee>();
+      this.projects = new Array<Project>();
+      this.selectedEmployee = new Employee(parsed.id, parsed.firstName, parsed.lastName, parsed.username, parsed.password, parsed.email, parsed.dateJoining, parsed.embg, parsed.role, parsed.projects);
+      this.projects = new Array<Project>();
+      this.employees = new Array<Employee>();
+      this.employeeService.getUsers().subscribe((text: Array<Employee>) => {
+        this.employees = text;
+      })
+      this.projectService.getProjects().subscribe((text: Array<Project>) => {
+        this.projects = text;
+      })
+      console.log(this.employees);
+      console.log(this.projects);
+    }
   }
-  createTime(){
-    var date = moment(this.date);
-    var currentDate = date.format('YYYY-MM-DD');
-    if(this.hours!=null && this.selectedEmployee.id!=null && this.selectedProject.id!=null){
+  createTime() {
+    if (this.haveData) {
+      var date = moment(this.date);
+      var currentDate = date.format('YYYY-MM-DD');
       this.timereportService.createTime(new Date(currentDate.toString()), this.hours, this.selectedEmployee.id, this.selectedProject.id).subscribe(text => {
         if (this.date != null && this.hours > 0) {
 
           this.dialogRef.close("Create");
           alert("Succesfull added timereport");
-          
+
+        }
+      });
+    }
+    else if (!this.haveData){
+      var date = moment(this.date);
+      var currentDate = date.format('YYYY-MM-DD');
+      this.timereportService.createTime(this.date, this.hours, this.selectedEmployee.id, this.selectedProject.id).subscribe(text => {
+        if (this.date != null && this.hours > 0) {
+
+          this.dialogRef.close("Create");
+          alert("Succesfull added timereport");
+
         }
       });
     }
@@ -84,19 +101,20 @@ export class NewtimereportComponent implements OnInit {
 
 
   onCloseCancel() {
-      this.dialogRef.close('Cancel');
+    this.dialogRef.close('Cancel');
   }
 
   changeProjectId($event) {
-    console.log($event);
     this.selectedProject = $event;
   }
 
   changeEmployee($event) {
-    console.log($event)
-    this.selectedEmployee= $event;
+    this.selectedEmployee = $event;
   }
-  changeHours($event){
+  changeHours($event) {
     this.hours = $event;
+  }
+  changeDate($event){
+    this.date=$event;
   }
 }
